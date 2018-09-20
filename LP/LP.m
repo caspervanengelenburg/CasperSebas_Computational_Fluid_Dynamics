386e501f69791485e6472ed8929ba13a0107d051
jacoblont <lontjacob@hotmail.com>
2018-09-20 11:59:06 +0200

    Github setup + first Matlab file

---
+++ LP/LP.m
@@ -0,0 +1,16 @@
+%% Linear programming assignment
+% Course: SC42055 Optimization in Systems and Control
+% Jacob Lont, 4424409 and Casper van Engelenburg, 4237080
+
+% E1, E2 and E3 are parameters changing from 0 to 18 for each group according to the sum of
+% the last three numbers of the student IDs:
+% E1 = Da1 + Db1, E2 = Da2 + Db2, E3 = Da3 + Db3,
+% where Da;3 is the right-most digit of one student and Db;3 is the right-most digit of the other
+% student.
+
+E1 = 4+0; E2 = 0+8; E3 = 9+0;
+
+
+
+
+

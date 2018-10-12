814ad595b802d89a826aa66f22035564adc1b7d6
caspervanengelenburg <40263235+caspervanengelenburg@users.noreply.github.com>
2018-10-12 10:48:16 +0200

    Add files via upload

---
+++ QP/SC42055_QP_4424409_4237080_20181017.m
@@ -0,0 +1,17 @@
+%% Quadratic programming assignment
+% Course: SC42055 Optimization in Systems and Control
+% Jacob Lont, 4424409 and Casper van Engelenburg, 4237080
+% Deadline: 17-10-2018
+
+
+%% Always first pull from Github before making any changes
+
+% E1, E2 and E3 are parameters changing from 0 to 18 for each group according to the sum of
+% the last three numbers of the student IDs:
+% E1 = Da1 + Db1, E2 = Da2 + Db2, E3 = Da3 + Db3,
+% where Da;3 is the right-most digit of one student and Db;3 is the right-most digit of the other
+% student.
+
+EE1 = 4+0; EE2 = 0+8; EE3 = 9+0;
+
+

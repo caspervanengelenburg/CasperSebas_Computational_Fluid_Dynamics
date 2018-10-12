e77f22fd139194ccb0136b995afc210742544580
caspervanengelenburg <c.c.j.vanengelenburg@student.tudelft.nl>
2018-10-12 14:26:46 +0200

    Update QP/SC42055_QP_4424409_4237080_20181017.m

--- QP/SC42055_QP_4424409_4237080_20181017.m
+++ QP/SC42055_QP_4424409_4237080_20181017.m
@@ -25,7 +25,8 @@ addpath(genpath(fileparts(which(mfilename))));
 
 EE1 = 4+0; EE2 = 0+8; EE3 = 9+0;
 
-
+num_meas = 101+EE1; %total number of measurements 
+dt = 3600; %time step
 
 
 %% 1.
@@ -47,25 +48,50 @@ read_prices = readtable(strcat(pwd, '\Data\inputPrices.csv'));
 % optimoptions(’quadprog’,’Algorithm’,’active-set’)
 % optimoptions(’quadprog’,’Algorithm’,’interior-point-convex’)
 % optimoptions(’quadprog’,’MaxIter’,100)
-x = quadprog(H,c,A,b,Aeq,Beq,lb,ub,x0,options)
+
+% x = quadprog(H,c,A,b,Aeq,Beq,lb,ub,x0,options)
  
+%heat transfer data
+Qin = table2array(read_meas(:,2)); %heat transfer tank in
+Qout = table2array(read_meas(:,3)); %heat transfer tank out
+Q = [Qin Qout]'; %array of both heat transfer 
+B = [1 -1]; %B matrix
+
+%temperature data
+T = table2array(read_meas(:,4)); %tank temperature T_k
+Tamb = table2array(read_meas(:,5)); %ambient temperature T_amb
+Tk1 = table2array(read_meas(2:end,4)); %tank temperature time step ahead T_{k+1}
+
+%temperature differences
+DTamb = T - Tamb; %difference between tank temperature and ambient temperature T_k - T_amb
+DTk1 = Tk1 - T(1:end-1); %difference between tank temperatures per time step T_{k+1) - T_k
+
+%determine H and c matrices 
+H = zeros(2,2);
+Hd = zeros(2,2);
+c = zeros(2,1);
+cd = zeros(2,1);
 
 
+for idx = 1: num_meas
+    
+    Hd = dt^2* [ (DTamb(idx))^2            B*Q(:,idx)*DTamb(idx);
+                 B*Q(:,idx)*DTamb(idx)          (B*Q(:,idx))^2    ];
+    H = H + Hd;
+    
+    cd = dt*[2*DTk1(idx)*DTamb(idx);
+             -2*DTk1(idx)*B*Q(:,idx)];
+    c = c + cd;
+end
 
+%optimize the variables
+A = [];
+b = [];
+Aeq = [];
+Beq = [];
+lb = [0 0];
+ub = [inf inf];
+a0 = [];
+o = optimoptions('quadprog', 'Algorithm', 'interior-point-convex');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a = quadprog(H,c,A,b,Aeq,Beq,lb,ub,a0,o);
